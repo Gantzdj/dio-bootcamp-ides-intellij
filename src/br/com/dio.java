bddf10d914dbb69505c3400da89a4af4f9bedc35
Bruno Lima <119960631+Gantzdj@users.noreply.github.com>
2023-03-20 01:05:30 -0300

    first commit

---
+++ src/br/com/dio.java
@@ -0,0 +1,15 @@
+package br.com;
+
+import br.com.model.Gato;
+
+public class dio {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+      /* int a = 7;
+        int b = 3;
+        System.out.println("Hello world! "+ (a+b));*/
+    }
+}
+
